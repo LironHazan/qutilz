72819fc00c3a4b5fe931b80930ba8ca590c2b1fe
lironh <lironenk@gmail.com>
2021-11-08 15:32:52 +0200

    refactor(*): reporter local txt report file

--- src/reporter/reporter.ts
+++ src/reporter/reporter.ts
@@ -2,51 +2,71 @@ import { Project, ts } from 'ts-morph';
 import * as fs from 'fs';
 import * as path from 'path';
 
+/**
+ * Recursively grab all files
+ * @param currentDirPath
+ */
 function grabFilesSync(currentDirPath: string): string[] {
-  return fs
-    .readdirSync(currentDirPath)
-    .reduce((acc: string[], name: string) => {
-      const filePath = path.join(currentDirPath, name);
-      const stat = fs.statSync(filePath);
-      if (stat.isFile()) {
-        acc.push(filePath);
-      }
-      return acc;
-    }, []);
+    const files: string[] = [];
+
+    const walker = (_path: string, cb: (path: string) => void) => {
+        fs.readdirSync(_path).forEach((name: string) => {
+            const filePath = path.join(_path, name);
+            const stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                if (filePath.match(/.*.spec.ts|.*.test.ts/)) {
+                    cb(filePath);
+                }
+            } else if (stat.isDirectory()) {
+                walker(filePath, cb);
+            }
+        });
+    };
+
+    walker(currentDirPath, (filePath) => {
+        files.push(filePath);
+    });
+    return files;
 }
-const fgGreen = '\x1b[32m';
 
 /**
  * Parse a test file, grab all descriptions of suites/tests from the describe() + it() expressions
  * and reformat to a report
  */
 async function testsReporter(): Promise<void> {
-  console.log('Printing report', fgGreen);
-  const project = new Project();
-  const files = grabFilesSync('./'); // grab all files from local folder
-  for (const filePath of files) {
-    project.addSourceFileAtPath(filePath);
-    const sourceFile = project.getSourceFileOrThrow(filePath);
-    sourceFile
-      .getDescendantsOfKind(ts.SyntaxKind.CallExpression)
-      .forEach(ce => {
-        ce.getDescendantsOfKind(ts.SyntaxKind.Identifier).forEach(id => {
-          if (id.getText() === 'describe' || id.getText() === 'it') {
-            id.getNextSiblings().forEach(sibling => {
-              sibling
-                .getChildrenOfKind(ts.SyntaxKind.StringLiteral)
-                .forEach(str => {
-                  if (id.getText() === 'describe') {
-                    console.log('suite:', str.getText(), fgGreen);
-                  } else {
-                    console.log('test:', str.getText());
-                  }
-                });
+    const files = grabFilesSync('./'); // grab all files from local folder
+    const tests: Set<string> = new Set();
+    const project = new Project();
+    for (const filePath of files) {
+        console.log('Making a report for: ', filePath);
+        project.addSourceFileAtPath(filePath);
+        const sourceFile = project.getSourceFileOrThrow(filePath);
+        sourceFile.getDescendantsOfKind(ts.SyntaxKind.CallExpression).forEach((ce) => {
+            ce.getDescendantsOfKind(ts.SyntaxKind.Identifier)
+                .filter(id => id.getText() === 'describe' || id.getText() === 'it')
+                .forEach((id) => {
+                    id.getNextSiblings()
+                        .forEach((sibling) => {
+                        sibling.getChildrenOfKind(ts.SyntaxKind.StringLiteral)
+                            .forEach((str) => {
+                            if (id.getText() === 'describe') {
+                                tests.add(`[suite]: ${str.getText()}`)
+                            }
+                            else if (id.getText() === 'it') {
+                                tests.add(`[test]: ${str.getText()}`);
+                            }
+                        });
+                    });
             });
-          }
         });
-      });
-  }
+    }
+    const total = Array.from(tests).filter(test => test.includes('[test]')).length;
+    const print = `Total: ${total} tests:
+    ${Array.from(tests).join('\n')}`;
+    const specFile = project.createSourceFile('tests_report.txt', writer =>
+        writer.writeLine(print)
+    );
+    await specFile.save();
 }
 
 testsReporter().then();
